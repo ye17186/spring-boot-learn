ae700e202b15e5293d95c4b8529a418d67bac92f
yemeng-lhq <yemeng-lhq@sinosig.com>
2019-03-22 11:25:20 +0800

    first

---
+++ yclouds-bl-parent/YClouds-Service-Demo/src/main/java/com/yclouds/service/demo/YCloudsServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yclouds.service.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YCloudsServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YCloudsServiceDemoApplication.class, args);
+    }
+
+}
