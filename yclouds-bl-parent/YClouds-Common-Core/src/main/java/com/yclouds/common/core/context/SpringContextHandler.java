b8d228a328ff5bb58c32fbe79fdf423f89902690
yemeng-lhq <yemeng-lhq@sinosig.com>
2019-03-22 17:04:03 +0800

    Spring Context自动初始化

---
+++ yclouds-bl-parent/YClouds-Common-Core/src/main/java/com/yclouds/common/core/context/SpringContextHandler.java
@@ -0,0 +1,26 @@
+package com.yclouds.common.core.context;
+
+import com.yclouds.common.core.utils.SpringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * 自动初始化Spring Context、
+ * <br>
+ * 具体获取Bean操作，还是依赖SpringUtils
+ *
+ * @author yemeng-lhq
+ * @version 2019/3/22 16:44
+ * @see SpringUtils
+ */
+@Slf4j
+public class SpringContextHandler implements ApplicationContextAware {
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringUtils.initContext(applicationContext);
+        log.info("Spring Context自动初始化完成，可以通过SpringUtils类提供的方法来获取Bean.");
+    }
+}
---
+++ yclouds-bl-parent/YClouds-Service-Demo/src/main/java/com/yclouds/service/demo/config/AsyncConfig.java
@@ -0,0 +1,58 @@
+package com.yclouds.service.demo.config;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.Executor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * @author yemeng-lhq
+ * @version 2018/8/21 18:31
+ */
+@Slf4j
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    /**
+     * 线程执行器配置
+     *
+     * @return 线程Executor
+     */
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        // 线程池核心大小
+        executor.setCorePoolSize(10);
+        // 最大线程数
+        executor.setMaxPoolSize(50);
+        // 线程名前缀
+        executor.setThreadNamePrefix("AsyncTask-");
+        executor.initialize();
+        return executor;
+    }
+
+    /**
+     * 异常处理器
+     *
+     * @see org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler
+     */
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncExceptionHandler();
+    }
+
+    /**
+     * 自定义异常处理
+     */
+    private class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+        @Override
+        public void handleUncaughtException(Throwable throwable, Method method, Object... objects) {
+
+            log.error("Unexpected exception occurred invoking async method: " + method, throwable);
+        }
+    }
+}
