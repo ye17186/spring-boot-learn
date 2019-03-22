26fe7a6186139b1320e5951a2a6ec8b41540f9cd
yemeng-lhq <yemeng-lhq@sinosig.com>
2019-03-22 14:05:34 +0800

    Utils

---
+++ yclouds-bl-parent/YClouds-Common-Core/src/main/java/com/yclouds/common/core/request/ApiReq.java
@@ -0,0 +1,14 @@
+package com.yclouds.common.core.request;
+
+import java.io.Serializable;
+
+/**
+ * Api统一请求参数基类
+ *
+ * @author ye17186
+ * @version 2019/2/20 13:51
+ */
+public class ApiReq implements Serializable {
+
+    private static final long serialVersionUID = 928076086138882852L;
+}
--- yclouds-bl-parent/YClouds-Common-Core/src/main/java/com/yclouds/common/core/utils/StringUtils.java
+++ yclouds-bl-parent/YClouds-Common-Core/src/main/java/com/yclouds/common/core/utils/StringUtils.java
@@ -4,6 +4,7 @@ package com.yclouds.common.core.utils;
  * @author ye17186
  * @version 2018/6/29 12:12
  */
+@SuppressWarnings("unused")
 public class StringUtils extends org.apache.commons.lang3.StringUtils {
 
     private StringUtils() {
