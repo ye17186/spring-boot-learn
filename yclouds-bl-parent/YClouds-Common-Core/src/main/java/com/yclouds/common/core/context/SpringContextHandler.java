85a3373fb2ea1313a2d69207756c894f0ef1adbf
yemeng-lhq <yemeng-lhq@sinosig.com>
2019-03-25 17:05:56 +0800

    SpringUtils

--- yclouds-bl-parent/YClouds-Common-Core/src/main/java/com/yclouds/common/core/context/SpringContextHandler.java
+++ yclouds-bl-parent/YClouds-Common-Core/src/main/java/com/yclouds/common/core/context/SpringContextHandler.java
@@ -7,9 +7,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 /**
- * 自动初始化Spring Context、
+ * 自动初始化Spring Context
  * <br>
  * 具体获取Bean操作，还是依赖SpringUtils
+ * <br>
+ * 可通知@EnableSpringContext注解，启用本功能
  *
  * @author ye17186
  * @version 2019/3/22 16:44
