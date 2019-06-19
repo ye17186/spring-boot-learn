501126c25e97e9748f2c165d908c130cb7fd4afe
yemeng-lhq <yemeng-lhq@sinosig.com>
2019-06-19 11:32:33 +0800

    File  入口类名修正

--- Demo-File/src/main/java/com/yclouds/demo/file/DemoFileApplication.java
+++ Demo-File/src/main/java/com/yclouds/demo/file/DemoFileApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoUploadApplication {
+public class DemoFileApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoUploadApplication.class, args);
+        SpringApplication.run(DemoFileApplication.class, args);
     }
 
 }
